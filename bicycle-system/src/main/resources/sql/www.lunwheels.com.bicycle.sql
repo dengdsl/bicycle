662b9e6ae9752605b4f4038fc965941417fc772b
dsl <2564888970@qq.com>
2025-02-15 23:42:41 +0800

    type：导出 desc：导出sql脚本

--- bicycle-system/src/main/resources/sql/www.lunwheels.com.bicycle.sql
+++ bicycle-system/src/main/resources/sql/www.lunwheels.com.bicycle.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 08/02/2025 23:12:14
+ Date: 15/02/2025 23:41:44
 */
 
 SET NAMES utf8mb4;
@@ -23,6 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `bicycle_info`;
 CREATE TABLE `bicycle_info`  (
   `id` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'id',
+  `pro_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '品名',
   `model` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '型号',
   `image` varchar(10240) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'X光图片',
   `frame_no` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '车架号',
@@ -30,18 +31,14 @@ CREATE TABLE `bicycle_info`  (
   `qrcode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '二维码编码',
   `qr_img` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '二维码',
   `conclusion` tinyint(1) NULL DEFAULT NULL COMMENT '结论：1-通过；0-不通过',
+  `hollow_hole` tinyint(0) NOT NULL DEFAULT 1 COMMENT '空孔：1-合格；0-不合格',
+  `in_fold` tinyint(0) NOT NULL DEFAULT 1 COMMENT '内折：1-合格；1-不合格',
+  `raveling` tinyint(0) NOT NULL DEFAULT 1 COMMENT '乱纱：1-合格；0-不合格',
   `remark` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `is_del` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除：0-未删除，1-已删除',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  `delete_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of bicycle_info
--- ----------------------------
-INSERT INTO `bicycle_info` VALUES ('BY20250208279369', '1', 'http://www.server.lunwheels.com/static/images/2025/02/08/1739056612banner1.jpg;http://www.server.lunwheels.com/static/images/2025/02/08/1739068001banner2.jpg', '123456789456123', '2025-02-07 16:00:00', 'AX48GEOQ3ODV7EWA78I3V20MIGMPQHJP', '/static/qrcode/2025/02/08/AX48GEOQ3ODV7EWA78I3V20MIGMPQHJP.png', 1, '测试数据', 0, '2025-02-08 15:04:31', '2025-02-08 15:04:31', NULL);
 
 -- ----------------------------
 -- Table structure for sys_config
@@ -64,25 +61,25 @@ CREATE TABLE `sys_config`  (
 -- ----------------------------
 -- Records of sys_config
 -- ----------------------------
-INSERT INTO `sys_config` VALUES (1, 2, 'sys', 'filings', '网站IC备案链接', 'https://www.server.lunwheels.com', '网站备案链接', '2024-01-10 22:44:41', '2025-02-08 23:06:56');
-INSERT INTO `sys_config` VALUES (2, 5, 'sys', 'webLogo', '登录页LOGO', 'http://www.server.lunwheels.com/static/config/2025/02/08/1739086782logo.png', '登录界面LOGO，建议宽高比例为320:43', '2024-01-10 22:44:41', '2025-02-08 22:56:56');
+INSERT INTO `sys_config` VALUES (1, 2, 'sys', 'filings', '网站IC备案链接', 'https://beian.miit.gov.cn/#/Integrated/index', '网站备案链接', '2024-01-10 22:44:41', '2025-02-12 09:05:18');
+INSERT INTO `sys_config` VALUES (2, 5, 'sys', 'webLogo', '登录页LOGO', 'https://www.server.lunwheels.com/static/config/2025/02/08/1739101614logo.png', '登录界面LOGO，建议宽高比例为320:43', '2024-01-10 22:44:41', '2025-02-08 23:49:35');
 INSERT INTO `sys_config` VALUES (3, 1, 'sys', 'webName', '登录页系统名称', '银斯贝自行车管理系统', '网站名称', '2024-01-10 22:44:41', '2025-01-17 22:49:45');
-INSERT INTO `sys_config` VALUES (4, 6, 'sys', 'webFavicon', '网站LOGO', 'http://www.server.lunwheels.com/static/config/2025/02/08/1739095040favicon.png', '网站logo', '2024-01-10 22:44:41', '2025-02-08 22:11:26');
+INSERT INTO `sys_config` VALUES (4, 6, 'sys', 'webFavicon', '网站LOGO', 'https://www.server.lunwheels.com/static/config/2025/02/08/1739112466favicon.png', '网站logo', '2024-01-10 22:44:41', '2025-02-08 23:49:35');
 INSERT INTO `sys_config` VALUES (5, 3, 'qrcode', 'qrcodeWidth', '二维码生成的宽度', '400', '生成的二维码宽度', '2025-01-15 23:26:10', '2025-01-17 22:49:09');
 INSERT INTO `sys_config` VALUES (6, 4, 'qrcode', 'qrcodeHeight', '二维码生成的高度', '400', '生成的二维码高度', '2025-01-15 23:26:26', '2025-01-17 22:49:14');
 INSERT INTO `sys_config` VALUES (7, 3, 'sys', 'theme', '管理后台主题颜色', '#2682FC', '系统主题颜色，必须为十六进制颜色，例如#2682FC', '2025-01-15 23:27:13', '2025-01-17 23:36:49');
-INSERT INTO `sys_config` VALUES (8, 2, 'banner', 'bannerImgs', '公众号查询界面图片', '[{\"src\":\"http://www.server.lunwheels.com/static/config/2025/02/08/1739059241banner1.jpg\",\"sort\":1},{\"src\":\"http://www.server.lunwheels.com/static/config/2025/02/08/1739041442banner2.jpg\",\"sort\":2}]', '轮播图列表，列表项中必须包含src属性，建议图片宽高比为4：3', '2025-01-15 23:27:44', '2025-02-08 22:50:15');
-INSERT INTO `sys_config` VALUES (9, 4, 'sys', 'loginBg', '管理后台登录背景图', 'http://www.server.lunwheels.com/static/config/2025/02/08/17390690491737405237loginbg.png', '登录界面背景图', '2025-01-15 23:30:10', '2025-02-08 23:00:36');
+INSERT INTO `sys_config` VALUES (8, 2, 'banner', 'bannerImgs', '公众号查询界面图片', '[{\"src\":\"https://www.server.lunwheels.com/static/config/2025/02/08/1739106060banner1.jpg\",\"sort\":1},{\"src\":\"https://www.server.lunwheels.com/static/config/2025/02/14/1739582382标准.jpg\",\"sort\":1}]', '轮播图列表，列表项中必须包含src属性，建议图片宽高比为4：3', '2025-01-15 23:27:44', '2025-02-14 16:28:20');
+INSERT INTO `sys_config` VALUES (9, 4, 'sys', 'loginBg', '管理后台登录背景图', 'https://www.server.lunwheels.com/static/config/2025/02/08/17391249161737405237loginbg.png', '登录界面背景图', '2025-01-15 23:30:10', '2025-02-08 23:49:35');
 INSERT INTO `sys_config` VALUES (10, 5, 'qrcode', 'qrcodeFontSize', '二维码上文字大小', '16', '生成的二维码图片下方的文字大小', '2025-01-16 14:44:28', '2025-01-17 22:49:18');
 INSERT INTO `sys_config` VALUES (11, 6, 'qrcode', 'qrcodeMargin', '二维码距离边框的距离', '0', '二维码和文字的间距', '2025-01-16 14:44:59', '2025-01-18 23:37:58');
 INSERT INTO `sys_config` VALUES (12, 1, 'sys', 'serverUrl', '二维码上传服务器地址', 'https://www.server.lunwheels.com', '文件上传时保存的服务器地址', '2025-01-16 15:15:41', '2025-02-08 23:06:56');
 INSERT INTO `sys_config` VALUES (13, 1, 'official', 'mobileTheme', '公众号主题色', '#2682FC ', '公众号查询界面的主题色，必须为十六进制颜色，例如#2682FC', '2025-01-16 15:22:53', '2025-01-19 00:54:34');
 INSERT INTO `sys_config` VALUES (14, 2, 'sys', 'filePath', '二维码上传路径', '/home/server/static', '文件上传时保存的文件夹路径', '2025-01-16 15:26:58', '2025-01-19 16:41:55');
 INSERT INTO `sys_config` VALUES (15, 1, 'banner', 'autoplay', '轮播图播放速度', '3000', '单位为毫秒，当设置为0时不自动轮播', '2025-01-18 13:52:07', '2025-01-20 09:06:06');
-INSERT INTO `sys_config` VALUES (16, 1, 'sys', 'filingsName', '网站IC备案号', 'XXX备 123456789-1号', 'IC备案号，例如：XXX备 123456789号', '2025-01-19 19:10:59', '2025-01-19 19:38:49');
-INSERT INTO `sys_config` VALUES (17, 1, 'sys', 'loginFooterBg', '页脚背景图', 'http://www.server.lunwheels.com/static/config/2025/02/08/17391146401737398251winspace.png', '登录界面底部背景图片，宽高比例建议为1920:200', '2025-01-20 14:38:36', '2025-02-08 23:00:36');
+INSERT INTO `sys_config` VALUES (16, 1, 'sys', 'filingsName', '网站IC备案号', '闽ICP备11002234号-9', 'IC备案号，例如：XXX备 123456789号', '2025-01-19 19:10:59', '2025-02-12 09:04:03');
+INSERT INTO `sys_config` VALUES (17, 1, 'sys', 'loginFooterBg', '页脚背景图', 'https://www.server.lunwheels.com/static/config/2025/02/08/17391228081737398251winspace.png', '登录界面底部背景图片，宽高比例建议为1920:200', '2025-01-20 14:38:36', '2025-02-08 23:49:35');
 INSERT INTO `sys_config` VALUES (18, 1, 'excel', 'headerBgColor', '表头背景颜色', '0x2682FC ', '下载的导入模版表头背景颜色，必须使用0x开头或#号开头的十六进制开头颜色，例如0x2682FC或者#2682FC ', '2025-01-21 13:54:13', '2025-01-21 16:51:42');
-INSERT INTO `sys_config` VALUES (19, 1, 'excel', 'templateDescription', '模版说明', ' *1.型号：请在下拉选项中进行选择，如果不选择或不按配置项填写将会导致导入失败！;\n *2.车架号：不能出现重复的车架号和已经录入系统的车架号，否则将导致导入失败！;\n*3.X光图片：图片不能嵌入单元格，只需要插入即可，否则会导致图片信息提取不到！;\n*4.生产日期：需要为日期格式，例如：2025/1/20！;\n*5.结论：请通过下拉选项选择，如果不进行选择默认为通过！;\n*6.备注：备注信息不能超过1024个字符！;\n7.数据编号和二维码编号以及二维码由系统自动生成！;', '模版说明，用于对导入格式进行描述，多条描述之间使用英文分号隔开，描述信息默认显示蓝色，如果要显示为红色，在每一条描述前面添加*号即可', '2025-01-21 13:58:35', '2025-01-21 19:26:46');
+INSERT INTO `sys_config` VALUES (19, 1, 'excel', 'templateDescription', '模版说明', '*1.型号：请在下拉选项中进行选择，如果不选择或不按配置项填写将会导致导入失败！;\n*2.车架号：不能出现重复的车架号和已经录入系统的车架号，否则将导致导入失败！;\n*3.X光图片：图片不能嵌入单元格，只需要插入即可，否则会导致图片信息提取不到！;\n*4.生产日期：需要为日期格式，例如：2025/1/20！;\n*5.结论：请通过下拉选项选择，如果不进行选择默认为通过！;\n*6.空孔：请通过下拉选项选择，如果不进行选择默认为合格！;\n*7.内折：请通过下拉选项选择，如果不进行选择默认为合格！;\n*8.乱纱：请通过下拉选项选择，如果不进行选择默认为合格！;\n*9.备注：备注信息不能超过1024个字符！;\n10.数据编号和二维码编号以及二维码由系统自动生成！;', '模版说明，用于对导入格式进行描述，多条描述之间使用英文分号隔开，描述信息默认显示蓝色，如果要显示为红色，在每一条描述前面添加*号即可', '2025-01-21 13:58:35', '2025-02-15 21:07:46');
 INSERT INTO `sys_config` VALUES (20, 1, 'excel', 'headerTextColor', '表头字体颜色', '0xffffff', '表头字体颜色，默认为白色，必须使用0x开头的十六进制颜色，例如0x2682FC或者#2682FC ', '2025-01-21 14:00:54', '2025-01-21 16:51:09');
 INSERT INTO `sys_config` VALUES (21, 1, 'excel', 'headerRowHeight', '导入模板描述单元格高度', '200', '批量导入模板第一行描述信息行高，默认为200', '2025-01-21 22:08:25', '2025-01-21 22:08:25');
 
@@ -106,7 +103,7 @@ CREATE TABLE `sys_dept`  (
 -- ----------------------------
 -- Records of sys_dept
 -- ----------------------------
-INSERT INTO `sys_dept` VALUES (1, NULL, '银斯贝', 0, '超级管理员', '18888888888', 0, '2025-01-09 14:22:34', '2025-01-09 14:22:34');
+INSERT INTO `sys_dept` VALUES (1, NULL, '厦门银贝斯体育用品有限公司', 0, '超级管理员', '18888888888', 0, '2025-01-09 14:22:34', '2025-02-12 01:06:34');
 
 -- ----------------------------
 -- Table structure for sys_dept_role
@@ -136,7 +133,12 @@ CREATE TABLE `sys_dept_user`  (
   INDEX `fk_user_id`(`user_id`) USING BTREE,
   CONSTRAINT `fk_dept_user_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `sys_dept` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `fk_dept_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户部门关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户部门关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_dept_user
+-- ----------------------------
+INSERT INTO `sys_dept_user` VALUES (1, 1, 2);
 
 -- ----------------------------
 -- Table structure for sys_dict
@@ -153,13 +155,16 @@ CREATE TABLE `sys_dict`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `dict_name`(`dict_name`) USING BTREE,
   UNIQUE INDEX `dict_type`(`dict_type`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '字典类型表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '字典类型表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_dict
 -- ----------------------------
 INSERT INTO `sys_dict` VALUES (1, '型号', 'model', 1, '自行车类型', '2025-01-19 02:29:26', '2025-01-19 02:29:26');
 INSERT INTO `sys_dict` VALUES (2, '结论', 'conclusion', 1, '结论：0-不通过，1-通过', '2025-01-19 02:40:21', '2025-01-19 02:40:21');
+INSERT INTO `sys_dict` VALUES (7, '空孔', 'hollowHole', 1, '1-合格；0-不合格', '2025-02-15 21:12:24', '2025-02-15 21:12:24');
+INSERT INTO `sys_dict` VALUES (8, '内折', 'inFold', 1, '1-合格；0-不合格', '2025-02-15 21:12:51', '2025-02-15 21:12:51');
+INSERT INTO `sys_dict` VALUES (9, '乱纱', 'raveling', 1, '1-合格；0-不合格', '2025-02-15 21:13:14', '2025-02-15 21:13:14');
 
 -- ----------------------------
 -- Table structure for sys_dict_data
@@ -176,14 +181,23 @@ CREATE TABLE `sys_dict_data`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `fk_dict_type`(`dict_type`) USING BTREE,
   CONSTRAINT `fk_dict_type` FOREIGN KEY (`dict_type`) REFERENCES `sys_dict` (`dict_type`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '字典数据表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '字典数据表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_dict_data
 -- ----------------------------
-INSERT INTO `sys_dict_data` VALUES (1, 'model', '公路自行车', 1, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (1, 'model', '车架', 1, 1, '', 0);
 INSERT INTO `sys_dict_data` VALUES (2, 'conclusion', '通过', 1, 1, '', 0);
 INSERT INTO `sys_dict_data` VALUES (3, 'conclusion', '不通过', 0, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (7, 'model', '前叉', 2, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (8, 'model', '把手', 3, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (9, 'model', '轮组', 4, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (10, 'hollowHole', '合格', 1, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (11, 'hollowHole', '不合格', 0, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (12, 'inFold', '合格', 1, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (13, 'inFold', '不合格', 0, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (14, 'raveling', '合格', 1, 1, '', 0);
+INSERT INTO `sys_dict_data` VALUES (15, 'raveling', '不合格', 0, 1, '', 0);
 
 -- ----------------------------
 -- Table structure for sys_menu
@@ -284,11 +298,12 @@ CREATE TABLE `sys_role`  (
   `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_role
 -- ----------------------------
+INSERT INTO `sys_role` VALUES (1, '超级管理员', 0, 0, 0, '等级仅次于admin', '2025-02-10 01:16:54');
 
 -- ----------------------------
 -- Table structure for sys_role_menu
@@ -303,8 +318,66 @@ CREATE TABLE `sys_role_menu`  (
   INDEX `fk_menu_id_role_menu`(`menu_id`) USING BTREE,
   CONSTRAINT `fk_menu_id_role_menu` FOREIGN KEY (`menu_id`) REFERENCES `sys_menu` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `fk_role_id_role_menu` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色菜单关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 56 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色菜单关联表' ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES (1, 1, 2);
+INSERT INTO `sys_role_menu` VALUES (2, 1, 3);
+INSERT INTO `sys_role_menu` VALUES (3, 1, 4);
+INSERT INTO `sys_role_menu` VALUES (4, 1, 5);
+INSERT INTO `sys_role_menu` VALUES (5, 1, 6);
+INSERT INTO `sys_role_menu` VALUES (6, 1, 7);
+INSERT INTO `sys_role_menu` VALUES (7, 1, 8);
+INSERT INTO `sys_role_menu` VALUES (8, 1, 12);
+INSERT INTO `sys_role_menu` VALUES (9, 1, 13);
+INSERT INTO `sys_role_menu` VALUES (10, 1, 14);
+INSERT INTO `sys_role_menu` VALUES (11, 1, 15);
+INSERT INTO `sys_role_menu` VALUES (12, 1, 16);
+INSERT INTO `sys_role_menu` VALUES (13, 1, 17);
+INSERT INTO `sys_role_menu` VALUES (14, 1, 18);
+INSERT INTO `sys_role_menu` VALUES (15, 1, 19);
+INSERT INTO `sys_role_menu` VALUES (16, 1, 20);
+INSERT INTO `sys_role_menu` VALUES (17, 1, 21);
+INSERT INTO `sys_role_menu` VALUES (18, 1, 25);
+INSERT INTO `sys_role_menu` VALUES (19, 1, 42);
+INSERT INTO `sys_role_menu` VALUES (20, 1, 43);
+INSERT INTO `sys_role_menu` VALUES (21, 1, 114);
+INSERT INTO `sys_role_menu` VALUES (22, 1, 115);
+INSERT INTO `sys_role_menu` VALUES (23, 1, 116);
+INSERT INTO `sys_role_menu` VALUES (24, 1, 117);
+INSERT INTO `sys_role_menu` VALUES (25, 1, 118);
+INSERT INTO `sys_role_menu` VALUES (26, 1, 119);
+INSERT INTO `sys_role_menu` VALUES (27, 1, 120);
+INSERT INTO `sys_role_menu` VALUES (28, 1, 121);
+INSERT INTO `sys_role_menu` VALUES (29, 1, 122);
+INSERT INTO `sys_role_menu` VALUES (30, 1, 123);
+INSERT INTO `sys_role_menu` VALUES (31, 1, 124);
+INSERT INTO `sys_role_menu` VALUES (32, 1, 125);
+INSERT INTO `sys_role_menu` VALUES (33, 1, 126);
+INSERT INTO `sys_role_menu` VALUES (34, 1, 127);
+INSERT INTO `sys_role_menu` VALUES (35, 1, 128);
+INSERT INTO `sys_role_menu` VALUES (36, 1, 129);
+INSERT INTO `sys_role_menu` VALUES (37, 1, 130);
+INSERT INTO `sys_role_menu` VALUES (38, 1, 131);
+INSERT INTO `sys_role_menu` VALUES (39, 1, 132);
+INSERT INTO `sys_role_menu` VALUES (40, 1, 133);
+INSERT INTO `sys_role_menu` VALUES (41, 1, 134);
+INSERT INTO `sys_role_menu` VALUES (42, 1, 135);
+INSERT INTO `sys_role_menu` VALUES (43, 1, 136);
+INSERT INTO `sys_role_menu` VALUES (44, 1, 137);
+INSERT INTO `sys_role_menu` VALUES (45, 1, 138);
+INSERT INTO `sys_role_menu` VALUES (46, 1, 139);
+INSERT INTO `sys_role_menu` VALUES (47, 1, 140);
+INSERT INTO `sys_role_menu` VALUES (48, 1, 141);
+INSERT INTO `sys_role_menu` VALUES (49, 1, 142);
+INSERT INTO `sys_role_menu` VALUES (50, 1, 143);
+INSERT INTO `sys_role_menu` VALUES (51, 1, 144);
+INSERT INTO `sys_role_menu` VALUES (52, 1, 145);
+INSERT INTO `sys_role_menu` VALUES (53, 1, 146);
+INSERT INTO `sys_role_menu` VALUES (54, 1, 147);
+INSERT INTO `sys_role_menu` VALUES (55, 1, 148);
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -324,12 +397,14 @@ CREATE TABLE `sys_user`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE,
   UNIQUE INDEX `account`(`account`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, 'admin', '123456', 'http://www.server.lunwheels.com/static/avatar/2025/02/08/1739099108favicon.png', '2564888970@qq.com', 'admin', 0, '超级管理员', '2024-02-29 20:38:34', '2025-02-08 14:49:52');
+INSERT INTO `sys_user` VALUES (1, 'admin', '123456', 'https://www.server.lunwheels.com/static/avatar/2025/02/10/1739180181favicon.png', '2564888970@qq.com', 'admin', 0, '超级管理员', '2024-02-29 20:38:34', '2025-02-10 00:58:51');
+INSERT INTO `sys_user` VALUES (2, 'adminer', '123456', 'https://www.server.lunwheels.com/static/avatar/2025/02/10/1739190825favicon.png', '', 'adminer', 0, '', '2025-02-10 01:17:22', '2025-02-10 01:17:22');
+
 -- ----------------------------
 -- Table structure for sys_user_role
 -- ----------------------------
@@ -343,7 +418,11 @@ CREATE TABLE `sys_user_role`  (
   INDEX `fk_role_id`(`role_id`) USING BTREE,
   CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色关联表' ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES (1, 2, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
